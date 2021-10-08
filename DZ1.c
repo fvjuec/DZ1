a19a0388de70a2b3b01c232940ff9c00063a44d4
fvjuec <91313158+fvjuec@users.noreply.github.com>
2021-10-08 11:19:41 +0300

    Create DZ1.c

---
+++ DZ1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void) {
+    int a = 0;
+    char n[32];
+    scanf("%s", &n); //ввод
+    for(int i = 0; i < strlen(n); i++) {
+        if(n[i] == '1' || n[i] == '0') {
+            if(n[i] == '1') {
+                a = a + pow(2, (strlen(n) - i - 1));
+            }
+        }
+        else {
+            printf("error");
+            exit(0);
+        }
+    }
+    printf("%d\n", a);
+    return 0;
+}
