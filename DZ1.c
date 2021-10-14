8a765946d6b01918053add4bd61f17e3cd4190ec
fvjuec <91313158+fvjuec@users.noreply.github.com>
2021-10-14 15:46:25 +0300

    Update DZ1.c

--- DZ1.c
+++ DZ1.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(void) {
     int a = 0;
     char n[32];
-    scanf("%s", &n); //ввод
-    for(int i = 0; i < strlen(n); i++) {
-        if(n[i] == '1' || n[i] == '0') {
-            if(n[i] == '1') {
-                a = a + pow(2, (strlen(n) - i - 1));
+    if(scanf("%s", &n) == 1) {
+        for(int i = 0; i < strlen(n); i++) {
+            if(n[i] == '1' || n[i] == '0') {
+                if(n[i] == '1') {
+                    a = a + pow(2, (strlen(n) - i - 1));
+                }
+            }
+            else {
+                printf("error");
+                exit(0);
             }
         }
-        else {
-            printf("error");
-            exit(0);
-        }
+        printf("%d\n", a);
     }
-    printf("%d\n", a);
     return 0;
 }
